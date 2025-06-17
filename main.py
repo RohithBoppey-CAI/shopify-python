e8c3d28df0ff1db9e5b7981e83b05e5d06a9e9ee
RohithBoppey <rohith.boppey@couture.ai>
2025-06-17 17:09:31 +0530

    update: port for deployment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from utils.shopify import get_all_product_ids
+import os
+import uvicorn
+
 app = FastAPI()
 
 
@@ -7,7 +10,13 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
 @app.post("/get-product-catalogue")
 async def get_product_catalogue(request: dict):
     result = get_all_product_ids(request)
     return result
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
